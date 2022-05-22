79474fcb714c2959cea377d224d80be490456b0a
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-05-22 10:11:20 +0900

    json dumps init

--- app.py
+++ app.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 
 @app.route('/stock', methods=['GET'])
 def get_stock():
-    vgm_url = 'https://minkabu.jp/financial_item_ranking/dividend_yield'
+    vgm_url = 'https://minkabu.jp/financial_item_ranking/per?exchange=tokyo.prime&order=asc&page=6'
     html_text = requests.get(vgm_url).text
     soup = BeautifulSoup(html_text, 'html.parser')
+    # 一種類ごとに取得して辞書型の配列を作成
+    stocks_name = [n.get_text() for n in soup.select('tbody tr td div.fwb')]
+    stocks_code = [n.get_text() for n in soup.select('tbody tr td div.md_sub')]
+    stocks_price = [re.sub(r'\(.*\)$', '', n.get_text()) for n in soup.select('tbody tr td div.wsnw')]
+    stocks_per = [re.sub('(\s|\r\n)', '', n.get_text()) for n in soup.select('tbody tr td.tar.cur.vamd')]
+    stocks_info = []
+    for name, code, price, per in zip(stocks_name, stocks_code, stocks_price, stocks_per):
+        stocks_info.append({"name": name, "code": code, "price": price, "per": per})
 
-    stock_names = [n.get_text() for n in soup.select('tbody tr td div.fwb')]
-    stock_price = [re.sub(r'\(.*\)$', '', n.get_text()) for n in soup.select('tbody tr td div.wsnw')]
-    print(stock_names)
-    print(stock_price)
-
-    return json.dumps(stock_price)
+    return json.dumps(stocks_info, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
     app.run()

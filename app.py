70a0f145270fb90ab963314b2869a94242d8d0f2
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-05-31 22:37:38 +0900

    fallList api add

--- app.py
+++ app.py
@@ -27,5 +27,22 @@ def get_stock():
 
     return json.dumps(stocks_info, ensure_ascii=False, indent=2)
 
+@app.route('/', methods=['GET'])
+def get_fall_list():
+    vgm_url = 'https://minkabu.jp/financial_item_ranking/fall?exchange=tokyo.prime&order=asc'
+    html_text = requests.get(vgm_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    # 一種類ごとに取得して辞書型の配列を作成
+    stocks_name = [n.get_text() for n in soup.select('tbody tr td div.fwb')]
+    stocks_code = [n.get_text() for n in soup.select('tbody tr td div.md_sub')]
+    stocks_price = [re.sub(r'\(.*\)$', '', n.get_text()) for n in soup.select('tbody tr td div.wsnw')]
+    stocks_fall = [re.sub('(\s|\r\n)', '', n.get_text()) for n in soup.select('tbody tr td.tar.cur.vamd')]
+    stocks_target_price = [n.get_text() for n in soup.select('tbody tr td.num.vamd a > span')]
+    stocks_info = []
+    for name, code, price, fall, target in zip(stocks_name, stocks_code, stocks_price, stocks_fall, stocks_target_price):
+        stocks_info.append({"name": name, "code": code, "price": price, "stockFall": fall, "stockTargetPrice": target})
+
+    return json.dumps(stocks_info, ensure_ascii=False, indent=2)
+
 if __name__ == "__main__":
     app.run()
